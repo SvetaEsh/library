3678e9baf831a747ad879e20740520fca9b74f49
Sviatlana E <Svetic010289@gmail.com>
2023-07-27 21:03:42 +0300

    add bootstrap in setting

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'catalog',
 ]
 
@@ -82,6 +84,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 
 # Password validation
